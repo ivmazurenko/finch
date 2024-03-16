3f7e986b22a3a3b402790abdc24e7f97144bb84d
imazurenko <ivmazurenko@yandex.ru>
2024-03-16 09:17:41 +0200

    Rename before add sql server

--- Finch.Npgsql.MockingDatabase/mock_database_postgre.dockerfile
+++ Finch.Npgsql.MockingDatabase/mock_database_postgre.dockerfile
@@ -3,4 +3,4 @@ ENV POSTGRES_DB mock_database
 ENV POSTGRES_USER sa
 ENV POSTGRES_PASSWORD sa_pass_111
 
-COPY init.sql /docker-entrypoint-initdb.d/
+COPY mock_database_postgre.sql /docker-entrypoint-initdb.d/
--- Finch.Npgsql.MockingDatabase/mock_database_postgre.sh
+++ Finch.Npgsql.MockingDatabase/mock_database_postgre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-container_name="postgres_mock_container"
-image_name="postgres_mock_image"
+container_name="mock_database_postgre_container"
+image_name="mock_database_postgre_image"
 
 if docker ps -a | grep -q "$container_name"; then
   echo "Stopping and removing existing container: $container_name"
@@ -9,5 +9,5 @@ if docker ps -a | grep -q "$container_name"; then
   docker rm "$container_name"
 fi
 
-docker build -t $image_name .
+docker build -t $image_name  --file mock_database_postgre.dockerfile .
 docker run -d --name "$container_name" -p 5432:5432  $image_name
